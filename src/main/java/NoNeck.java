7f81c1bf474f595aa7b4e1deaf26be3acfe85a46
RNJH224 <e1354204@u.nus.edu>
2026-01-25 21:27:27 +0800

    Level 0: Renamed Duke to NoNeck. Added *.class to gitignore

---
+++ src/main/java/NoNeck.java
@@ -0,0 +1,8 @@
+public class NoNeck {
+    public static void main(String[] args) {
+        String logo = "NoNeck";
+        System.out.println("Hello i'm " + logo);
+        System.out.println("what can i do for you");
+        System.out.println("\nBye, Hope to see you again soon!");
+    }
+}
