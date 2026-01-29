3435f553886a3db9d68b68d9b6a4a3d95a73d980
RNJH224 <e1354204@u.nus.edu>
2026-01-29 23:55:47 +0800

    level 2: Add , list

--- src/main/java/NoNeck.java
+++ src/main/java/NoNeck.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class NoNeck {
     public static void main(String[] args) {
         String logo = "NoNeck";
         String line;
+        int i = 0;
+        String[] storedLine = new String[100];
         System.out.println("Hello i'm " + logo);
         System.out.println("what can i do for you");
 
@@ -19,8 +22,15 @@ public class NoNeck {
                 bai = true;
                 System.out.println("Astalavista baby");
             }
+            else if (line.equals("repeat")) {
+                for (int j = 0; storedLine[j] != null; j++) {
+                    System.out.println(storedLine[j]);
+                }
+            }
             else {
+                storedLine[i] = i+1 + ") " + line;
                 System.out.println(line);
+                i++;
             }
         }
     }
