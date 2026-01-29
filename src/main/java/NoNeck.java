067af886801aeb4e255c39416317f7b3d9cf502f
RNJH224 <e1354204@u.nus.edu>
2026-01-29 23:36:41 +0800

    level 1: echo done

--- src/main/java/NoNeck.java
+++ src/main/java/NoNeck.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class NoNeck {
     public static void main(String[] args) {
         String logo = "NoNeck";
+        String line;
         System.out.println("Hello i'm " + logo);
         System.out.println("what can i do for you");
-        System.out.println("\nBye, Hope to see you again soon!");
+
+        boolean bai = false;
+        while (!bai) {
+
+
+
+            Scanner sc = new Scanner(System.in);
+            line = sc.nextLine();
+
+            if (line.equals("bye")) {
+                bai = true;
+                System.out.println("Astalavista baby");
+            }
+            else {
+                System.out.println(line);
+            }
+        }
     }
 }
