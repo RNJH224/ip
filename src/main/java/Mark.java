070382a9cfbcb087f32217b45df082e7030e7fc7
RNJH224 <e1354204@u.nus.edu>
2026-01-30 15:08:44 +0800

    level 3: mark and unmark

---
+++ src/main/java/Mark.java
@@ -0,0 +1,6 @@
+public class Mark {
+    public static String[] line = new String[100];
+    public static boolean[] marked = new boolean[100];
+
+
+}
--- src/main/java/NoNeck.java
+++ src/main/java/NoNeck.java
@@ -6,14 +6,14 @@ public class NoNeck {
         String logo = "NoNeck";
         String line;
         int i = 0;
-        String[] storedLine = new String[100];
+
         System.out.println("Hello i'm " + logo);
         System.out.println("what can i do for you");
 
         boolean bai = false;
+        boolean[] mark = new boolean[100];
+
         while (!bai) {
-
-
 
             Scanner sc = new Scanner(System.in);
             line = sc.nextLine();
@@ -22,13 +22,35 @@ public class NoNeck {
                 bai = true;
                 System.out.println("Astalavista baby");
             }
+            else if (line.trim().startsWith("mark")) {
+                int pos = line.indexOf("mark")+4;
+                String rest = line.substring(pos).trim();
+                int num = Integer.parseInt(rest);
+                System.out.println("marked as done");
+                System.out.println(Mark.line[num-1]);
+                Mark.marked[num-1] = true;
+            }
+            else if (line.trim().startsWith("unmark")) {
+                int pos = line.indexOf("unmark")+6;
+                String rest = line.substring(pos).trim();
+                int num = Integer.parseInt(rest);
+                System.out.println("not done yet");
+                System.out.println(Mark.line[num-1]);
+                Mark.marked[num-1] = false;
+            }
             else if (line.equals("list")) {
-                for (int j = 0; storedLine[j] != null; j++) {
-                    System.out.println(storedLine[j]);
+                for (int j = 0; Mark.line[j] != null; j++) {
+                    System.out.print(Mark.line[j]);
+                    if (!Mark.marked[j]) {
+                        System.out.println("[ ]");
+                    } else {
+                        System.out.println("[x]");
+                    }
                 }
             }
             else {
-                storedLine[i] = i+1 + ") " + line;
+                Mark.line[i] = i+1 + ") " + line;
+                Mark.marked[i] = false;
                 System.out.println("added: " + line);
                 i++;
             }
